318b31cf7b8f9d770ce500b36c4a25f1b0f4bc46
Estela Parrado <estela.parrado@gmail.com>
2018-04-07 21:12:58 +0200

    list of items

---
+++ README.md
@@ -0,0 +1,9 @@
+Receta de Risotto de setas (vegano)
+
+Con los datos que nos facilita el mercado, usaremos un JSON de la receta del rissoto de setas, tenemos que hacer un pequeño frontal con el nombre del plato, y la lista para comprar los ingredientes teniendo en cuenta que:
+
+Podré seleccionar todos los ingredientes, alguno o ninguno
+Podré cambiar la cantidad de un artículo
+Siempre tendré una lista actualizada de lo que me costaría hacer un pedido (en caso de tener elementos seleccionados)
+El botón de compra siempre marcará dicha cantidad
+Desarrollaremos usando JS Vanilla
---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict'
+
+let infoRecipe = {};
+const JSON_RECIPE = 'https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json';
+const pageTitle = document.querySelector('.page-title');
+const ingredientList = document.querySelector('.ingredient-list');
+
+function getInfo () {
+  fetch(JSON_RECIPE)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    let infoRecipe = json.recipe;
+
+    pageTitle.innerHTML = infoRecipe.name;
+
+    for (const ingredient of infoRecipe.ingredients) {
+      ingredientList.innerHTML += `<li class="ingredient-item"><input type="checkbox" name="ingredient-selected">
+        <input type="text" name="ingredient-quantity" class="items" value="1" min="0"></input>
+        <div class="ingredient-info">
+          <h3 class="product">${ingredient.product}</h3>
+          <h4 class="brand">${ingredient.brand ? ingredient.brand : ''}</h4>
+          <p class="quantity">${ingredient.quantity}</p>
+        </div>
+        <p class="price">${ingredient.price.toFixed(2)} €</p>
+      </li>`
+    }
+  });
+}
+
+window.onload = getInfo;
